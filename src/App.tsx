a59098bdd5b809e5066259d3ab307a6d4c932c23
Phoebe DG <phoebevdgash@gmail.com>
2020-01-06 23:48:54 +0000

    first commit - initial setup and look and feel landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home } from "./HomePage/Home.component";
+import phoebepassportphoto from "./static/phoebepassportphoto.jpeg";
+import "./App.css";
 
-const App: React.FC = () => {
+export const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="app__header">
+        <h1 className="app__title">phoebedg</h1>
+        <img className="app__image" src={phoebepassportphoto} alt="passport" />
+        <p className="app__arrow">⇟⇟</p>
+      </div>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </Fragment>
   );
-}
-
-export default App;
+};
---
+++ src/HomePage/Home.component.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Home.css";
+
+export const Home = () => {
+  return (
+    <div className="home">
+      <h2 className="home__title">About</h2>
+    </div>
+  );
+};
