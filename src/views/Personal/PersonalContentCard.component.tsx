3a6950bd1a00eba0a15c98dd6045a3ec349ff9cd
Phoebe DG <phoebevdgash@gmail.com>
2020-02-06 23:13:28 +0000

    tidy project

--- src/views/Personal/PersonalContentCard.component.tsx
+++ src/views/Personal/PersonalContentCard.component.tsx
@@ -1,13 +1,7 @@
 import React from "react";
-import "./Personal.css";
+import "./personal.css";
 import { PersonalContentCardProps } from "./personal.types";
 
-export const PersonalContentCard = ({
-  children
-}: PersonalContentCardProps) => {
-  return (
-    <div className="personal__content-card">
-      {children}
-    </div>
-  );
+export const PersonalContentCard = ({ children }: PersonalContentCardProps) => {
+  return <div className="personal__content-card">{children}</div>;
 };
--- src/views/Personal/Quotes.component.tsx
+++ src/views/Personal/Quotes.component.tsx
@@ -1,22 +1,44 @@
 import React, { Fragment } from "react";
-import "./Personal.css";
+import "./personal.css";
 
 export const Quotes = () => {
   return (
     <Fragment>
       <span>"What is left to us women, but the realms of the unknown?"</span>
       <span className="personal__citation">- Philippa Gregory</span>
-      <br/>
-      <span>“Before we were conceived, we existed in part as an egg in our mother's ovary.</span>
-      <span>All the eggs a woman will ever carry form in her ovaries while she is a four-month-old fetus in the womb of her mother.</span>
-      <span>This means our cellular life as an egg begins in the womb of our grandmother.</span>
-      <span>Each of us spent five months in our grandmother's womb and she in turn formed within the womb of her grandmother.</span>
-      <span>We vibrate to the rhythms of our mother's blood before she herself is born.</span>
-      <span>And this pulse is the thread of blood that runs all the way back through the grandmothers to the first mother.”</span>
+      <br />
+      <span>
+        “Before we were conceived, we existed in part as an egg in our mother's
+        ovary.
+      </span>
+      <span>
+        All the eggs a woman will ever carry form in her ovaries while she is a
+        four-month-old fetus in the womb of her mother.
+      </span>
+      <span>
+        This means our cellular life as an egg begins in the womb of our
+        grandmother.
+      </span>
+      <span>
+        Each of us spent five months in our grandmother's womb and she in turn
+        formed within the womb of her grandmother.
+      </span>
+      <span>
+        We vibrate to the rhythms of our mother's blood before she herself is
+        born.
+      </span>
+      <span>
+        And this pulse is the thread of blood that runs all the way back through
+        the grandmothers to the first mother.”
+      </span>
       <span className="personal__citation">- Layne Redmond</span>
-      <br/>
-      <span>"Once you have tasted flight, you will forever walk the earth with your eyes turned skyward, for there you have been, and there you will always long to return.”</span>
+      <br />
+      <span>
+        "Once you have tasted flight, you will forever walk the earth with your
+        eyes turned skyward, for there you have been, and there you will always
+        long to return.”
+      </span>
       <span className="personal__citation">- Leonardo da Vinci</span>
     </Fragment>
-  )
-}
+  );
+};
