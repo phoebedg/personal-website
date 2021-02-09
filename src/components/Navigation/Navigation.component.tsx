52c6e3f9e7642c5beb482706512e6d60f5ab38ec
Phoebe Gash <phoebedg@Phoebes-MBP.cust.communityfibre.co.uk>
2021-02-09 21:23:40 +0000

    change nav bar to open when icon hovered

--- src/components/Navigation/Navigation.component.tsx
+++ src/components/Navigation/Navigation.component.tsx
@@ -16,12 +16,9 @@ export const Navigation: React.FC = () => {
   const [visible, setVisible] = useState<boolean>(false);
   const [hovered, setHovered] = useState<boolean>(false);
 
-  const toggleNav = (e: React.MouseEvent) => {
-    e.preventDefault();
-    setVisible(!visible);
-  };
-  const closeNav = (e: React.MouseEvent) => {
-    setVisible(false);
+  const handleOpenNav = () => {
+    setHovered(true);
+    setVisible(true);
   };
 
   const location = useLocation();
@@ -32,28 +29,31 @@ export const Navigation: React.FC = () => {
       : "navigation__link";
   const getNavStyles = (): string =>
     visible ? "navigation navigation-mobile-open" : "navigation";
-  const svgFill = visible ? "#d5621f" : hovered ? "#d5621f" : "#d1d7c9";
+  const svgFill = hovered ? "#d5621f" : "#d1d7c9";
 
   return (
     <div className={getNavStyles()}>
       <div
         className="navigation__icon"
         aria-label="nav-icon"
-        onMouseEnter={(e: React.MouseEvent) => setHovered(true)}
-        onMouseLeave={(e: React.MouseEvent) => setHovered(false)}
-        onClick={toggleNav}
+        onMouseEnter={handleOpenNav}
+        onTouchStart={handleOpenNav}
+        onMouseLeave={() => setHovered(false)}
       >
         <NavIcon fill={svgFill} />
       </div>
       {visible && (
-        <div className="navigation__modal" onClick={closeNav}>
+        <div
+          className="navigation__modal"
+          onClick={() => setVisible(false)}
+          onMouseLeave={() => setVisible(false)}
+        >
           <div className="navigation__menu">
             {navMenuItems.map(item => {
               return (
                 <Link
                   data-testid="nav-link"
                   className={getMenuItemStyles(item)}
-                  onClick={closeNav}
                   to={item === "home" ? "/" : `/${item}/#main`}
                   key={item}
                 >
