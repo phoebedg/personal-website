2662c1f7ac2a16b54dd369110eff52d57b24f479
Phoebe Gash <phoebevdgash@gmail.com>
2021-03-04 22:50:30 +0000

    modify press content

--- src/static/assets.ts
+++ src/static/assets.ts
@@ -297,25 +297,6 @@ export const pressLinks = [
     title: "Pollen Software Engineering Internship Interview 2019",
     href:
       "https://team.pollen.co/engineering/engineering-internships-at-pollen/"
-  },
-  {
-    title: "Her Campus Bristol Interview 2014",
-    href:
-      "https://www.hercampus.com/school/bristol/campus-celeb-phoebe-drinkall-face-oxford-fashion-week"
-  },
-  {
-    title: "The Tab Brookes Interview 2014",
-    href:
-      "https://thetab.com/uk/brookes/2014/03/05/brookes-fresher-face-of-oxford-fashion-week-2481"
-  },
-  {
-    title: "Oxford Mail article 2 2014",
-    href:
-      "https://www.oxfordmail.co.uk/news/11069028.oxford-fashion-week-face-oxford-model-cant-wait-next-catwalk-show-couture-show-video/"
-  },
-  {
-    title: "Oxford Mail article 2014",
-    href: "https://www.oxfordmail.co.uk/news/11030885.face-oxford/"
   }
 ];
   
