e008eeb02c7803eed6d2f9254490dd4fc4a489ec
Phoebe Gash <phoebevdgash@gmail.com>
2021-03-04 23:09:22 +0000

    tidy up professional

--- src/views/Professional/Professional.component.tsx
+++ src/views/Professional/Professional.component.tsx
@@ -23,16 +23,16 @@ export const Professional: React.FC = () => (
     <br />
     <span>~</span>
     <h4>Tools, Technologies & Methodolgies</h4>
-    <span>JavaScript, React, Redux, Typescript</span>
+    <span>JavaScript, Typescript, React, Redux</span>
     <span>Webpack, SASS, CSS, HTML</span>
-    <span>Golang, NodeJS, ExpressJS, Django, PostgreSQL</span>
+    <span>Go, Node, Express, Django, PostgreSQL</span>
     <span>
       Jest, Enzyme, react-testing-library, TestCafe, Pytest, PACT,
       BrowserStack, Chromatic, Storybook
     </span>
     <span>CircleCI, CI/CD, Heroku, Vercel</span>
     <span>AWS Cloudwatch, New Relic, Kibana, Postman</span>
-    <span>Git, Jira, Zeplin, Figma</span>
+    <span>Git, Jira</span>
     <span>Scrum, Kanban, Agile, TDD</span>
   </ContentWrap>
 );
